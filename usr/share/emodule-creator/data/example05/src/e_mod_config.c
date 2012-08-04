ac680c4a4bacf4aca47eba25bdadcc4a8606f338
Deon 'PrinceAMD' Thomas <princeamd.elive@gmail.com>
2012-08-04 00:28:32 -0400

    Updated Module:Example05 [Menu, Sub-Menu, Radio]

--- usr/share/emodule-creator/data/example05/src/e_mod_config.c
+++ usr/share/emodule-creator/data/example05/src/e_mod_config.c
@@ -1,99 +1,106 @@
-#include <e.h>
+#include "e.h"
 #include "e_mod_main.h"
 
-/* The typedef for this structure is declared inside the E code in order to
- * allow everybody to use this type, you dont need to declare the typedef, 
- * just use the E_Config_Dialog_Data for your data structures declarations */
-struct _E_Config_Dialog_Data 
+struct _E_Config_Dialog_Data
 {
-   int switch1;
+    E_Config_Dialog *cfd;
+    Evas_Object *obj;
+    int set;
 };
 
-/* Local Function Prototypes */
 static void *_create_data(E_Config_Dialog *cfd);
-static void _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static void  _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static void _cb_first_setting_change(void *data, Evas_Object *obj);
 static void _fill_data(E_Config_Dialog_Data *cfdata);
-static Evas_Object *_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
-static int _basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static int _basic_check_changed(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata);
 
-/* External Functions */
-/* Function for calling our personal dialog menu */
-E_Config_Dialog *
-e_int_config_HOHO_module(E_Container *con, const char *params) 
-{
-   E_Config_Dialog *cfd = NULL;
-   E_Config_Dialog_View *v = NULL;
-   char buf[4096];
-
-   /* is this config dialog already visible ? */
-   if (e_config_dialog_find("HOHO", "advanced/HOHO")) return NULL;
-
-   v = E_NEW(E_Config_Dialog_View, 1);
-   if (!v) return NULL;
-
-   v->create_cfdata = _create_data;
-   v->free_cfdata = _free_data;
-   v->basic.create_widgets = _basic_create;
-   v->basic.apply_cfdata = _basic_apply;
-
-   /* Icon in the theme */
-   snprintf(buf, sizeof(buf), "%s/e-module-HOHO.edj", HOHO_conf->module->dir);
-
-   /* create new config dialog */
-   cfd = e_config_dialog_new(con, D_("HOHO Module"), "HOHO", 
-                             "advanced/HOHO", buf, 0, v, NULL);
-
-   e_dialog_resizable_set(cfd->dia, 1);
-   HOHO_conf->cfd = cfd;
-   return cfd;
-}
-
-/* Local Functions */
 static void *
-_create_data(E_Config_Dialog *cfd) 
+_create_data(E_Config_Dialog *cfd)
 {
-   E_Config_Dialog_Data *cfdata = NULL;
+    E_Config_Dialog_Data *cfdata;
+   // Config_Item *ci;
 
-   cfdata = E_NEW(E_Config_Dialog_Data, 1);
-   _fill_data(cfdata);
-   return cfdata;
+    cfdata = E_NEW(E_Config_Dialog_Data, 1);
+   // ci = cfd->data;
+
+    _fill_data(cfdata);
+    //cfdata->cfd = cfd;
+    return cfdata;
 }
 
-static void 
-_free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata) 
+static void
+_free_data(E_Config_Dialog *cfd  __UNUSED__, E_Config_Dialog_Data *cfdata)
 {
-   HOHO_conf->cfd = NULL;
-   E_FREE(cfdata);
+    E_FREE(cfdata);
 }
 
-static void 
-_fill_data(E_Config_Dialog_Data *cfdata) 
+static void
+_fill_data(E_Config_Dialog_Data *cfdata)
 {
-   /* load a temp copy of the config variables */
-   cfdata->switch1 = HOHO_conf->switch1;
+    cfdata->set = skeletor_config->set;
 }
 
+static int
+_basic_apply_data(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
+{
+    skeletor_config->set = cfdata->set;
+    e_config_save_queue();
+    return 1;
+}
 static Evas_Object *
-_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata) 
+_basic_create_widgets(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data *cfdata)
 {
-   Evas_Object *o = NULL, *of = NULL, *ow = NULL;
+   Evas_Object *o, *ob, *of;
 
    o = e_widget_list_add(evas, 0, 0);
 
-   of = e_widget_framelist_add(evas, D_("General"), 0);
-   e_widget_framelist_content_align_set(of, 0.0, 0.0);
-   ow = e_widget_check_add(evas, D_("Use Switch 1"), 
-                           &(cfdata->switch1));
-   e_widget_framelist_object_append(of, ow);
-   e_widget_list_object_append(o, of, 1, 1, 0.5);
+   of = e_widget_framelist_add(evas, "General Settings", 0);
+   ob = e_widget_check_add(evas, "First setting to save", &(cfdata->set));
 
+   cfdata->obj = ob;
+   e_widget_on_change_hook_set(ob, _cb_first_setting_change, cfdata);
+   e_widget_framelist_object_append(of, ob);
+   e_widget_list_object_append(o, of, 1, 0, 0.5);
+
+   e_dialog_resizable_set(cfd->dia, 0);
    return o;
 }
 
-static int 
-_basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata) 
+void
+e_int_config_skeletor_module(E_Container *con, Config_Item *ci)
 {
-   HOHO_conf->switch1 = cfdata->switch1;
-   e_config_save_queue();
-   return 1;
+    E_Config_Dialog *cfd;
+    E_Config_Dialog_View *v;
+    char buf[PATH_MAX];
+
+    if(e_config_dialog_find("E", "utils/skeletor")) return;
+
+    v = E_NEW(E_Config_Dialog_View, 1);
+    if(!v) return;
+
+    v->create_cfdata = _create_data;
+    v->free_cfdata = _free_data;
+    v->basic.create_widgets = _basic_create_widgets;
+    v->basic.apply_cfdata = _basic_apply_data;
+
+    cfd = e_config_dialog_new(con, "Skeletor Settings", "E", "utils/skeletor",
+            "preferences-engine", 0, v, NULL);
+    e_dialog_resizable_set(cfd->dia, 1);
+    skeletor_config->cfd = cfd;
 }
+
+static void
+_cb_first_setting_change(void *data, Evas_Object *obj)
+{
+    E_Config_Dialog_Data *cfdata;
+    int val;
+
+    cfdata = data;
+    val = e_widget_check_checked_get(obj);
+
+    cfdata->set = val;
+    e_widget_check_checked_set(cfdata->obj,val);
+}
+
+
+
